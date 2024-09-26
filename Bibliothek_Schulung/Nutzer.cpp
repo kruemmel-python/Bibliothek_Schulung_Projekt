8dcce87f7be12733502c468d48759af5548f909c
Ralf Krümmel <ralf.kruemmel+python@outlook.de>
2024-09-26 12:00:37 +0200

    Update Nutzer.cpp

--- Bibliothek_Schulung/Nutzer.cpp
+++ Bibliothek_Schulung/Nutzer.cpp
@@ -1,20 +1,22 @@
 #include "Nutzer.hpp"
 
-Nutzer::Nutzer(const std::string& vorname, const std::string& nachname, const std::string& strasse, int hausnummer,
-    const std::string& plz, const std::string& ort, const std::string& email, const std::string& telefonnummer,
-    const std::string& mitgliedsnummer)
+using namespace std;
+
+Nutzer::Nutzer(const string& vorname, const string& nachname, const string& strasse, int hausnummer,
+    const string& plz, const string& ort, const string& email, const string& telefonnummer,
+    const string& mitgliedsnummer)
     : vorname(vorname), nachname(nachname), strasse(strasse), hausnummer(hausnummer), plz(plz), ort(ort),
     email(email), telefonnummer(telefonnummer), mitgliedsnummer(mitgliedsnummer) {}
 
-std::string Nutzer::getVorname() const {
+string Nutzer::getVorname() const {
     return vorname;
 }
 
-std::string Nutzer::getNachname() const {
+string Nutzer::getNachname() const {
     return nachname;
 }
 
-std::string Nutzer::getStrasse() const {
+string Nutzer::getStrasse() const {
     return strasse;
 }
 
@@ -22,22 +24,22 @@ int Nutzer::getHausnummer() const {
     return hausnummer;
 }
 
-std::string Nutzer::getPLZ() const {
+string Nutzer::getPLZ() const {
     return plz;
 }
 
-std::string Nutzer::getOrt() const {
+string Nutzer::getOrt() const {
     return ort;
 }
 
-std::string Nutzer::getEmail() const {
+string Nutzer::getEmail() const {
     return email;
 }
 
-std::string Nutzer::getTelefonnummer() const {
+string Nutzer::getTelefonnummer() const {
     return telefonnummer;
 }
 
-std::string Nutzer::getMitgliedsnummer() const {
+string Nutzer::getMitgliedsnummer() const {
     return mitgliedsnummer;
 }
